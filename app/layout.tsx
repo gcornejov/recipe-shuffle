fe81393b5df962c78dfdf00aa1351415f25e890a
gcornejov <g.cornejovivado@gmail.com>
2024-10-11 00:31:07 -0300

    Improve search module functionality and style

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="w-[1300px] m-auto shadow-2xl">{children}</div>
+      </body>
     </html>
   );
 }
